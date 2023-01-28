549312b1b30a8ee00870398c68b46885616a0d96
saikumarsmh <saikumarsmh@gmail.com>
2023-01-28 17:36:38 +0530

    pushing to master

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+echo "welcome" | tee guvi{1..8}.txt > /dev/null
 read -p "Enter the first Value :" var1
 read -p "Enter the second Value :" var2
 if [ $var1 == $var2 ]
@@ -7,6 +8,8 @@ git add *
 git commit -m "pushing to master"
 git push -u guvissh master
 else
+git checkout develop
+git rebase master
 git add *
 git commit -m "pusshing to develop"
 git push -u guvissh develop
